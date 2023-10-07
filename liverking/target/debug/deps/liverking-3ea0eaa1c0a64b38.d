f72dc65a812ac2bc89f15d4c89af706b4cd62936
Sebastian Strobl <sebastian@strobl.net>
2023-10-07 02:44:54 +0200

    Change: cargo.toml

--- liverking/target/debug/deps/liverking-3ea0eaa1c0a64b38.d
+++
@@ -1,8 +0,0 @@
-E:\Projects\Cuber\liverking\target\debug\deps\liverking-3ea0eaa1c0a64b38.exe: src\lib.rs src\hunt.rs src\juice.rs src\liver.rs
-
-E:\Projects\Cuber\liverking\target\debug\deps\liverking-3ea0eaa1c0a64b38.d: src\lib.rs src\hunt.rs src\juice.rs src\liver.rs
-
-src\lib.rs:
-src\hunt.rs:
-src\juice.rs:
-src\liver.rs:
